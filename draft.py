cf35f9cc83649d61f8ecbbd4b9a0f8de92c12d23
swati_goyal@outlook.com <be9entia>
2018-10-02 17:57:44 +0530

    Domestic flight booking test added

--- draft.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-
-
-current_date = datetime.now().strftime("%d %b, %a")
-next_date = (datetime.now() + relativedelta(day=2)).strftime("%d %b, %a")
-print(current_date)
-print(next_date)
